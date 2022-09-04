7c7eb0e160738db992a31eb67fa6099b665e1a15
yunheejo <rhcjrdbsgml@gmail.com>
2022-09-04 17:34:14 +0900

    props 예시

---
+++ Props/my-app/src/props01.js
@@ -0,0 +1,31 @@
+//props로 다양한것을 넘겨줄 수 있다
+import { useState } from "react";
+
+function App() {
+  const [] = useState("");
+  return (
+    <div>
+      <Btn text="Save Changes" big={true} />
+      <Btn text="Continue" big={false} />
+    </div>
+  );
+}
+
+function Btn({ text, big }) {
+  return (
+    <button
+      style={{
+        backgroundColor: "tomato",
+        color: "white",
+        padding: "10px 20px",
+        border: 0,
+        borderRadius: "2rem",
+        fontSize: big ? 18 : 16, //big={true} 18이고 big={false} 16이다
+      }}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default App;
