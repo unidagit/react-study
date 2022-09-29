0fbc62d9c00cc5d2c72363584b9b70d6c28994c7
yunheejo <rhcjrdbsgml@gmail.com>
2022-09-29 23:24:52 +0900

    오탈자 수정

--- coin/src/Router.tsx
+++ coin/src/Router.tsx
@@ -6,7 +6,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/:coinID" element={<Coin />}></Route>
+        <Route path="/:coinId" element={<Coin />}></Route>
         <Route path="/" element={<Coins />}></Route>
       </Routes>
     </BrowserRouter>
